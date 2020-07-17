8ca1924cfb9b28e3824df57175e049d052f8ff45
ctcuff <dev.ctcuff@gmail.com>
2020-07-17 17:59:00 -0400

    Reduced video size

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <video muted autoplay loop playsinline>
-      <source src="../assets/videos/beach.mp4" type="video/mp4" />
+      <!-- 
+        Video by Jess Vide from Pexels:
+        https://www.pexels.com/video/drone-footage-of-a-beach-near-city-buildings-4612067/
+      -->
+      <source src="../assets/videos/beach-1920x1080.mp4" type="video/mp4" />
     </video>
     <header>
       <div class="text-container">
